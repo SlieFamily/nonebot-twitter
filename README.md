de54fa02f421339cf5d02e8d1c3b10b667b1c342
kanomahoro <9173011623@njau.edu.cn>
2021-10-11 22:19:38 +0800

    v0.1

--- README.md
+++ README.md
@@ -1,2 +1,144 @@
-# nonebot-twitter
-基于nonebot2的twitter推送插件
+<!-- markdownlint-disable MD033 MD041-->
+<p align="center">
+  <img src="https://cdn.jsdelivr.net/gh/kanomahoro/images@main/logo.png" width="200" height="200"/>
+</p>
+
+<div align="center">
+
+# HanayoriBot(Twitter插件)
+<!-- markdownlint-disable-next-line MD036 -->
+_✨ 基于NoneBot2的Twitter推送插件，自带百度翻译接口 ✨_
+
+</div>
+
+## 简介
+
+本插件基于[NoneBot2](https://github.com/nonebot/nonebot2)与[go-cqhttp](https://github.com/Mrs4s/go-cqhttp),可以及时将Twitter用户的最新推文推送至群聊，并且自带基于百度翻译的推文翻译接口，及时跟进你所关注的Vtuber的外网动态。
+
+名字由来：花寄女子寮(Hanayori Joshiryou) 花寄天下第一！！！！！（来自某花寄~~DD~~单推人）
++ 鹿乃ちゃん：B站(316381099)
++ 小东人魚Official：B站(441382432)
++ 花丸晴琉Official：B站(441381282)
++ 野野宫のののOfficial：B站(441403698)
+
+## 特色
+
+1. **轻依赖**：本插件在编写时尽量避免了采用使用第三方包，以减少依赖项
+2. **轻量化**：本插件经由4个文件构成，可以快速集成至任何已有的机器人框架
+3. **支持aarch64架构**：本插件在树莓派4B上能够正常运行，并且支持安卓平台的termux环境
+4. **强权限管理**：本插件在编写时采用了强权限的设计，仅可由超级用户、群主、管理员进行操作
+5. **平行数据库**：私聊好友、各群聊拥有独立的数据库，互不干扰
+## 即刻开始
+### B站视频教程
+
+1. 前置教程
+[【【HanayoriBot】十分钟拥有你的群聊 单推(DD)机器人-哔哩哔哩】](https://b23.tv/PbPAqE)
+2. Twitter插件教程
+   待完善
+
+### 安装NoneBot2
+完整文档可以在 [这里](https://v2.nonebot.dev/) 查看。
+
+懒得看文档？下面是快速安装指南：
+
+1. (可选)使用你喜欢的 Python 环境管理工具创建新的虚拟环境。
+2. 使用 `pip` (或其他) 安装 NoneBot 脚手架。
+
+   ```bash
+   pip install nb-cli
+   ```
+
+3. 使用脚手架创建项目
+
+   ```bash
+   nb create
+   ```
+4. 请在创建项目时选用cqhttp适配器，并且按照文档完成最小实例的创建
+   
+### 配置文件示例
+1. .env
+   ```yml
+   ENVIRONMENT=prod
+   ```
+2. .env.prod
+   ```yml
+   HOST=127.0.0.1
+   PORT=8080
+   SECRET=
+   ACCESS_TOKEN=
+   SUPERUSERS=[超级用户账户(你的QQ号,不是机器人的账户)]
+   COMMAND_START=["","/"]
+   NICKNAME=["","/"]
+   COMMAND_SEP=["."]
+   ```
+3. 请务必安装以上示例配置你的Bot；go-cqhttp请自行参照官方文档配置
+### 安装HanayoriBot(Twitter插件)
+   1. pip安装
+   ```bash
+   pip install nonebot-plugin-twitter
+   ```
+   请在你的bot.py文件中加入以下内容
+   ```python
+   nonebot.load_plugin("nonebot_plugin_twitter")#添加此行
+   nonebot.load_from_toml("pyproject.toml")#位于本行前
+   ```
+   2. 使用nb-cli安装(推荐)
+  
+   在你的Bot目录下执行：
+   ```bash
+   nb plugin install nonebot_plugin_twitter
+   ```
+
+### 配置HanayoriBot(Twitter插件)
+如果您的服务器位于境外，请忽略以下内容
+1. 首先确保你的代理软件支持http代理模式，并且已经开启，不推荐启用全局代理模式
+2. 明确你的代理端口号，请咨询你的代理服务提供商
+3. 根据平台不同，请按照以下方式分别设置代理：
+   1. Windows平台 cmd环境
+   ```bash
+   set http_proxy=http://127.0.0.1:端口号  
+   set https_proxy=http://127.0.0.1:端口号  
+   ```
+   2. windows平台 PowerShell环境
+   ```bash
+   $env:HTTP_PROXY="127.0.0.1:端口号"  
+   $env:HTTP_PROXY="127.0.0.1:端口号" 
+   ```
+   3. Linux平台 Bash环境
+   ```bash
+   export http_proxy=http://127.0.0.1:端口号 
+   export https_proxy=http://127.0.0.1:端口号 
+   ```
+4. 在按照3设置代理后，请不要关闭终端，在当前终端执行nb run才能使机器人连上代理（请提前运行go-cqhttp）
+5. 在机器人成功运行后，会在机器人根目录会生成baidu_translate.json文件，若你不需要推文翻译功能，请忽略下一步
+6. 用文本编辑器打开baidu_translate.json
+   ```bash
+   {"appid": "填入你申请的百度翻译API的appid", "baidu_token": "填入你申请的百度翻译API的密钥"}
+   ```
+   按以上要求填写，申请可去[百度翻译开放平台](https://api.fanyi.baidu.com/)，申请通用翻译API即可
+### 指令说明
+以下所以指令在群聊中只允许超级用户(主人)、群主、管理员进行操作，私聊中不受限制
+**在群聊中使用格式**：@机器人 指令 推特ID(如果指令要求的话) 
+**在私聊中使用格式**：指令 推特ID(如果指令要求的话)
+**推特ID**：在Twitter的用户主页，@后面的部分；或者‘https://twitter.com/xxxxx’ 用户主页链接中的xxxxx
+**所有指令如下：**
+1. **推特关注 推特ID**
+   添加新用户
+2. **推特取关 推特ID**
+   取关用户
+3. **推特列表**
+   显示当前关注列表
+4. **开启翻译 推特ID**
+   开启推文翻译
+5. **关闭翻译 推特ID**
+   关闭推文翻译
+6.  **帮助**
+   顾名思义
+
+### 遇到问题？
+你可以直接提交issue，或者发送邮件到：kano@hanayori.top
+### 效果展示
+
+![效果1](https://cdn.jsdelivr.net/gh/kanomahoro/images@main/20211011_1.jpg)
+
+![效果2](https://cdn.jsdelivr.net/gh/kanomahoro/images@main/20211011_2.jpg)
---
+++ src/plugins/nonebot_plugin_twitter/__init__.py
@@ -0,0 +1,202 @@
+from dataclasses import MISSING
+from nonebot import on_command
+from nonebot import rule
+from nonebot import on_request
+from nonebot import on_notice
+from nonebot.adapters import Bot,Event
+from nonebot.adapters.cqhttp.event import MessageEvent, Status
+from nonebot.rule import to_me
+from nonebot.adapters.cqhttp.permission import GROUP_ADMIN, GROUP_OWNER, PRIVATE_FRIEND
+from nonebot.permission import SUPERUSER
+from nonebot.typing import T_State
+from nonebot.adapters.cqhttp import Bot,Message,GroupMessageEvent,bot,FriendRequestEvent,GroupRequestEvent,GroupDecreaseNoticeEvent
+from nonebot import require
+from . import data_source
+from . import model
+from . import config
+import asyncio
+import nonebot
+model.Init()
+config.token=data_source.init_token()
+tweet_index=0
+scheduler = require('nonebot_plugin_apscheduler').scheduler
+@scheduler.scheduled_job('interval',minutes=60,id='flush_token')
+async def flush():#定时刷新token
+    config.token=await data_source.get_token()
+@scheduler.scheduled_job('interval',seconds=20,id='tweet')
+async def tweet():#定时推送用户最新推文
+    if model.Empty():
+        return
+    (schedBot,)=nonebot.get_bots().values()
+    global tweet_index
+    users=model.GetUserList()
+    tweet_index %=len(users)
+    tweet_id,data=await data_source.get_latest_tweet(users[tweet_index][2],config.token)
+    if tweet_id=='' or users[tweet_index][3]==tweet_id:
+        tweet_index+=1
+        return
+    model.UpdateTweet(users[tweet_index][0],tweet_id)
+    text,translate,media,url=data_source.get_tweet_details(data)
+    media=await data_source.download_media(media)
+    translate=await data_source.baidu_translate(config.appid,translate,config.baidu_token)
+    cards=model.GetALLCard(users[tweet_index][0])
+    for card in cards:
+        if card[1]==1:#是群聊
+            if card[2]==1:#需要翻译
+                await schedBot.call_api('send_msg',**{
+                    'message':text+translate+media+url,
+                        'group_id':card[0]
+                })
+            else:#不需要翻译
+                await schedBot.call_api('send_msg',**{
+                        'message':text+media+url,
+                        'group_id':card[0]
+                })
+        else:#私聊
+            if card[2]==1:#需要翻译
+                await schedBot.call_api('send_msg',**{
+                    'message':text+translate+media+url,
+                    'user_id':card[0]
+                })
+            else:
+                await schedBot.call_api('send_msg',**{
+                    'message':text+media+url,
+                    'user_id':card[0]
+                })
+    tweet_index+=1
+adduser = on_command('推特关注',rule=to_me(),priority=5,permission=GROUP_ADMIN|GROUP_OWNER|PRIVATE_FRIEND|SUPERUSER,)
+@adduser.handle()#添加用户
+async def handle(bot: Bot, event: MessageEvent, state: T_State):
+    is_group=int(isinstance(event,GroupMessageEvent))
+    id=event.get_session_id()
+    if not id.isdigit():
+        id=id.split('_')[1]
+    args = str(event.get_message()).strip()
+    msg = '指令格式错误！请按照：推特关注 推特ID'
+    if args!='':
+        user=model.GetUserInfo(args)
+        if len(user)!=0:
+                status=model.AddCard(args,id,is_group)
+                if status==0:
+                    msg='{}({})添加成功！'.format(user[1],args) #待测试
+                else:
+                    msg='{}({})已存在！'.format(user[1],args) #待测试
+        else:
+            user_name,user_id=await data_source.get_user_info(args,config.token)
+            if(user_id!=''):
+                model.AddNewUser(args,user_name,user_id)
+                model.AddCard(args,id,is_group)
+                msg='{}({})添加成功！'.format(user_name,args) #待测试
+            else:
+                msg='{} 推特ID不存在或网络错误！'.format(args)
+    Msg = Message(msg)
+    await adduser.finish(Msg)
+    
+removeuser = on_command('推特取关',rule=to_me(),priority=5,permission=GROUP_ADMIN|GROUP_OWNER|PRIVATE_FRIEND|SUPERUSER,)
+@removeuser.handle()#取关用户
+async def handle(bot: Bot, event: MessageEvent, state: T_State):
+    is_group=int(isinstance(event,GroupMessageEvent))
+    id=event.get_session_id()
+    if not id.isdigit():
+        id=id.split('_')[1]
+    args = str(event.get_message()).strip()
+    msg = '指令格式错误！请按照：推特取关 UID'
+    if args!='':
+        user=model.GetUserInfo(args)
+        if len(user)==0:
+            msg = '{} 用户不存在！请检查推特ID是否错误'.format(args)
+        else:
+            status=model.DeleteCard(args,id,is_group)
+            if status!=0:
+                msg='{}({})不在当前群组/私聊关注列表'.format(user[1],args)
+            else:
+                msg='{}({})删除成功！'.format(user[1],args)
+    Msg = Message(msg)
+    await adduser.finish(Msg)
+
+alllist = on_command('推特列表',rule=to_me(),priority=5,permission=GROUP_ADMIN|GROUP_OWNER|PRIVATE_FRIEND|SUPERUSER,)
+@alllist.handle()#显示当前群聊/私聊中的关注列表
+async def handle(bot: Bot, event: MessageEvent, state: T_State):
+    is_group=int(isinstance(event,GroupMessageEvent))
+    id=event.get_session_id()
+    if not id.isdigit():
+        id=id.split('_')[1]
+    msg='用户名(推特ID)\n'
+    content=''
+    if not id.isdigit():
+        id=id.split('_')[1]
+    user=model.GetUserList()
+    for index in user:
+        card=model.GetCard(index[0],id,is_group)
+        if len(card)!=0:
+            content+='{}({})\n{}\n'.format(index[1],index[0],str(card[2])).replace('1','推文翻译：开').replace('0','推文翻译：关')
+    if content=='':
+        msg='当前群聊/私聊关注列表为空！'
+    else:
+        msg=msg+content
+    Msg=Message(msg)
+    await alllist.finish(Msg)
+
+ontranslate = on_command('开启翻译',rule=to_me(),priority=5,permission=GROUP_ADMIN|GROUP_OWNER|PRIVATE_FRIEND|SUPERUSER,)
+@ontranslate.handle()#开启推文翻译
+async def handle(bot: Bot, event: MessageEvent, state: T_State):
+    is_group=int(isinstance(event,GroupMessageEvent))
+    id=event.get_session_id()
+    if not id.isdigit():
+        id=id.split('_')[1]
+    args = str(event.get_message()).strip()
+    msg = '指令格式错误！请按照：开启翻译 推特ID'
+    if args!='':
+        user=model.GetUserInfo(args)
+        if len(user)==0:
+            msg = '{} 用户不存在！请检查UID是否错误'.format(args)
+        else:
+            card=model.GetCard(args,id,is_group)
+            if len(card)==0:
+                msg='{}({})不在当前群组/私聊关注列表！'.format(user[1],args)
+            else:
+                model.TranslateON(args,id,is_group)
+                msg='{}({})开启推文翻译！'.format(user[1],args)
+    Msg=Message(msg)
+    await ontranslate.finish(Msg)
+
+offtranslate = on_command('关闭翻译',rule=to_me(),priority=5,permission=GROUP_ADMIN|GROUP_OWNER|PRIVATE_FRIEND|SUPERUSER,)
+@offtranslate.handle()#关闭推文翻译
+async def handle(bot: Bot, event: MessageEvent, state: T_State):
+    is_group=int(isinstance(event,GroupMessageEvent))
+    id=event.get_session_id()
+    if not id.isdigit():
+        id=id.split('_')[1]
+    args = str(event.get_message()).strip()
+    msg = '指令格式错误！请按照：关闭翻译 推特ID'
+    if args!='':
+        user=model.GetUserInfo(args)
+        if len(user)==0:
+            msg = '{} 用户不存在！请检查UID是否错误'.format(args)
+        else:
+            card=model.GetCard(args,id,is_group)
+            if len(card)==0:
+                msg='{}({})不在当前群组/私聊关注列表！'.format(user[1],args)
+            else:
+                model.TranslateOFF(args,id,is_group)
+                msg='{}({})关闭推文翻译！'.format(user[1],args)
+    Msg=Message(msg)
+    await offtranslate.finish(Msg)
+
+help = on_command('推特帮助',rule=to_me(),priority=5,permission=GROUP_ADMIN|GROUP_OWNER|PRIVATE_FRIEND|SUPERUSER,)
+@help.handle()#启动动态推送
+async def handle(bot: Bot, event: MessageEvent, state: T_State):
+    menu='HanayoriBot(推特插件)目前支持的功能：\n(请将ID替换为需操作的推特ID，也即@后面的名称)\n推特关注 ID\n推特取关 ID\n推特列表\n开启翻译 ID\n关闭翻译 ID\n'
+    info='当前版本：v0.1\n作者：鹿乃ちゃんの猫\n反馈邮箱：kano@hanayori.top'
+    msg=menu+info
+    Msg=Message(msg)
+    await help.finish(Msg)
+
+group_decrease = on_notice(priority=5)
+@group_decrease.handle()
+async def _(bot: Bot, event: GroupDecreaseNoticeEvent, state: T_State):
+    id=event.get_session_id()
+    if not id.isdigit():
+        id=id.split('_')[1]
+    if event.self_id == event.user_id:
+        model.DeleteGroupCard(id)
